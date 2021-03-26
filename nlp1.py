18d35eb6b1ccce49ea077ecf386cf01f5729be74
UshaGayatri2020 <73848118+UshaGayatri2020@users.noreply.github.com>
2021-03-25 21:22:48 -0500

    Add files via upload

---
+++ nlp1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 22 16:18:37 2021
+
+@author: vmukku
+"""
+
+import os
+import pandas as pd
+os.chdir("C://Users")
+df_t1=pd.read_csv(r'/vmukku/Desktop/TableauCourse/DSclasses/News_Data.csv')
+df_t1.head()
